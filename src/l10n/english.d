15f319e82661dee379950d0463bac13e472049ef
Cordarion the Grey <CordarionTheGrey@ya.ru>
2017-09-21 04:29:32 +0700

    Recalculate distances on the map from the port
    Closes #4

---
+++ src/l10n/english.d
@@ -0,0 +1,5 @@
+module l10n.english;
+
+import dython;
+
+mixin(dythonizeFile!"src/l10n/english.dy");
---
+++ src/l10n/russian.d
@@ -0,0 +1,5 @@
+module l10n.russian;
+
+import dython;
+
+mixin(dythonizeFile!"src/l10n/russian.dy");
